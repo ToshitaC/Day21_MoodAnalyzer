15faaefb3f414faca120b4a2bc0f788d589025bf
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-17 20:56:59 +0530

    Added:Program for analyze mood Sad or happy

--- MoodAnalyzer.java
+++ MoodAnalyzer.java
@@ -1,7 +1,22 @@
 package com.bridgelabz;
 
 public class MoodAnalyzer {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Mood Analyzer Program!1");
+    //TC1.1 : Given "I am in Sad Mood" message Should Return SAD
+    //TC1.2 : Given "I am in Any Mood" message Should Return HAPPY
+    private String message;
+    public String analyzeMood(String mood) {
+        if(message.contains("sad"))
+            return "SAD";
+        else if(message.contains("any"))
+            return "HAPPY";
+        return mood;
+    }
+    //Default Constructor
+    public MoodAnalyzer() {
+
+    }
+    // Parameterized Constructor
+    public MoodAnalyzer(String message) {
+        this.message = message;
     }
 }
